0e34eb8913e353845f9c3c52cf27eba0bc95e603
David Salgado <david@digitalronin.com>
2019-10-15 15:43:19 +0100

    Add 'malformed-yaml' action
    Reject a PR if it contains malformed YAML files

---
+++ malformed-yaml/reject-malformed-yaml.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require "json"
+require "octokit"
+require "yaml"
+
+require File.join(File.dirname(__FILE__), "github")
+
+def malformed_yaml_files
+  yaml_files_in_pr.map do |file|
+    YAML.load File.read(file)
+    nil
+  rescue Psych::SyntaxError
+    file
+  end.compact
+end
+
+def yaml_files_in_pr
+  files_in_pr.grep(/\.(yaml|yml)$/)
+end
+
+############################################################
+
+files = malformed_yaml_files
+
+if files.any?
+  file_list = files.map {|f| "  * #{f}"}.join("\n")
+
+  message = <<~EOF
+  The following files contain malformed YAML:
+
+  #{file_list}
+
+  Please correct them and resubmit this PR.
+
+  EOF
+
+  reject_pr(message)
+end
