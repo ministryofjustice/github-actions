4f9fa1ad02e982779eb4660c4d928d3623c8b2ef
David Salgado <david@digitalronin.com>
2019-12-24 13:36:13 +0000

    Move functions inside GithubClient class

--- shared/github.rb
+++ shared/github.rb
@@ -11,14 +11,58 @@ class Executor
 end
 
 class GithubClient
-  attr_reader :client
+  attr_reader :client, :executor
 
-  def initialize
+  def initialize(args = {})
     unless ENV.key?("GITHUB_TOKEN")
       raise "No GITHUB_TOKEN env var found. Please make this available via the github actions workflow\nhttps://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret"
     end
 
+    @executor = args.fetch(:executor) { Executor.new }
     @client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+  end
+
+  def files_in_pr
+    pull_request_files
+      .map(&:filename)
+      .sort
+      .uniq
+  end
+
+  def commit_changes(message)
+    files = modified_files
+    if files.any?
+      puts "Committing changes to:\n  #{files.join("\n  ")}"
+      commit_files(branch, files, message)
+    end
+  end
+
+  # private
+
+  def create_blobs(files)
+    files.map do |file_name|
+      content = File.read(file_name)
+      blob_sha = client.create_blob(repo, Base64.encode64(content), "base64")
+      {path: file_name, mode: "100644", type: "blob", sha: blob_sha}
+    end
+  end
+
+  def modified_files
+    stdout, _stderr, _status = executor.execute("git status --porcelain=1 --untracked-files=no")
+
+    stdout
+      .split("\n")
+      .map { |line| line.sub(" M ", "") }
+  end
+
+  def commit_files(branch, files, commit_message)
+    ref = "heads/#{branch}"
+    sha_latest_commit = ref(repo, ref).object.sha
+    sha_base_tree = commit(repo, sha_latest_commit).commit.tree.sha
+    changes = create_blobs files
+    sha_new_tree = create_tree(repo, changes, {base_tree: sha_base_tree}).sha
+    sha_new_commit = create_commit(repo, commit_message, sha_new_tree, sha_latest_commit).sha
+    update_ref(repo, ref, sha_new_commit)
   end
 
   def event
@@ -29,13 +73,31 @@ class GithubClient
     @evt ||= JSON.parse File.read(ENV["GITHUB_EVENT_PATH"])
   end
 
+  def reject_pr(message)
+    puts "Requesting changes..."
+    puts message
+
+    client.create_pull_request_review(
+      repo,
+      pr_number,
+      {
+        body: message,
+        event: "REQUEST_CHANGES",
+      }
+    )
+  end
+
   def repo
     name = event.dig("repository", "name")
     owner = event.dig("repository", "owner", "login")
     [owner, name].join("/")
   end
 
-  def pull_request_files(repo, pr_number)
+  def pr_number
+    event.dig("pull_request", "number")
+  end
+
+  def pull_request_files
     client.pull_request_files(repo, pr_number)
   end
 
@@ -68,6 +130,8 @@ class GithubClient
   end
 end
 
+#############################################################################
+
 def github
   @github_client ||= GithubClient.new
 end
@@ -80,70 +144,16 @@ def repo
   github.repo
 end
 
-def pr_number
-  event.dig("pull_request", "number")
-end
-
 def branch
   event.dig("pull_request", "head", "ref")
-end
-
-def create_blobs(files)
-  files.map do |file_name|
-    content = File.read(file_name)
-    blob_sha = github.create_blob(repo, Base64.encode64(content), "base64")
-    {path: file_name, mode: "100644", type: "blob", sha: blob_sha}
-  end
-end
-
-def files_in_pr
-  github.pull_request_files(repo, pr_number)
-    .map(&:filename)
-    .sort
-    .uniq
 end
 
 def execute(cmd)
   Executor.new.execute(cmd)
 end
 
-def commit_changes(message)
-  files = modified_files
-  if files.any?
-    puts "Committing changes to:\n  #{files.join("\n  ")}"
-    commit_files(branch, files, message)
-  end
-end
-
-def modified_files
-  stdout, _stderr, _status = execute("git status --porcelain=1 --untracked-files=no")
-
-  stdout
-    .split("\n")
-    .map { |line| line.sub(" M ", "") }
-end
-
-def commit_files(branch, files, commit_message)
-  ref = "heads/#{branch}"
-  sha_latest_commit = github.ref(repo, ref).object.sha
-  sha_base_tree = github.commit(repo, sha_latest_commit).commit.tree.sha
-  changes = create_blobs files
-  sha_new_tree = github.create_tree(repo, changes, {base_tree: sha_base_tree}).sha
-  sha_new_commit = github.create_commit(repo, commit_message, sha_new_tree, sha_latest_commit).sha
-  github.update_ref(repo, ref, sha_new_commit)
-end
-
 def reject_pr(message)
-  puts "Requesting changes..."
-  puts message
-
-  github.create_pull_request_review(
-    repo,
-    pr_number,
-    {
-      body: message,
-      event: "REQUEST_CHANGES",
-    }
-  )
+  github.reject_pr(message)
   exit 1
 end
+
