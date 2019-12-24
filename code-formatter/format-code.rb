0a7c9c888371c41e89a2ec7206d0e7a348aa59f4
David Salgado <david@digitalronin.com>
2019-12-24 11:10:15 +0000

    Move code formatter code into CodeFormatter class

--- code-formatter/format-code.rb
+++ code-formatter/format-code.rb
@@ -6,10 +6,4 @@ require "octokit"
 require File.join(File.dirname(__FILE__), "github")
 require File.join(File.dirname(__FILE__), "code_formatter")
 
-def main
-  format_terraform_code
-  format_ruby_code
-  commit_changes "Commit changes made by code formatters"
-end
-
-main
+CodeFormatter.new.run
--- shared/code_formatter.rb
+++ shared/code_formatter.rb
@@ -1,28 +1,36 @@
+class CodeFormatter
+  def run
+    format_terraform_code
+    format_ruby_code
+    commit_changes "Commit changes made by code formatters"
+  end
 
-def format_terraform_code
-  terraform_directories_in_pr.each do |dir|
-    execute "terraform fmt #{dir}"
+  private
+
+  def format_terraform_code
+    terraform_directories_in_pr.each do |dir|
+      execute "terraform fmt #{dir}"
+    end
+  end
+
+  def format_ruby_code
+    ruby_files_in_pr.each do |file|
+      execute "standardrb --fix #{file}" if FileTest.exists?(file)
+    end
+  end
+
+  def terraform_directories_in_pr
+    terraform_files_in_pr
+      .map { |f| File.dirname(f) }
+      .sort
+      .uniq
+  end
+
+  def ruby_files_in_pr
+    files_in_pr.grep(/\.rb$/)
+  end
+
+  def terraform_files_in_pr
+    files_in_pr.grep(/\.tf$/)
   end
 end
-
-def format_ruby_code
-  ruby_files_in_pr.each do |file|
-    execute "standardrb --fix #{file}" if FileTest.exists?(file)
-  end
-end
-
-def terraform_directories_in_pr
-  terraform_files_in_pr
-    .map { |f| File.dirname(f) }
-    .sort
-    .uniq
-end
-
-def ruby_files_in_pr
-  files_in_pr.grep(/\.rb$/)
-end
-
-def terraform_files_in_pr
-  files_in_pr.grep(/\.tf$/)
-end
-
