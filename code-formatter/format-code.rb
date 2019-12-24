0f109b6f29cb052b741347a20091b2bb78b30d7c
David Salgado <david@digitalronin.com>
2019-12-24 10:55:12 +0000

    Move code formatter functions to a library file
    This is a pre-requisite for adding some unit tests

--- code-formatter/format-code.rb
+++ code-formatter/format-code.rb
@@ -4,36 +4,12 @@ require "json"
 require "octokit"
 
 require File.join(File.dirname(__FILE__), "github")
+require File.join(File.dirname(__FILE__), "code_formatter")
 
-def format_terraform_code
-  terraform_directories_in_pr.each do |dir|
-    execute "terraform fmt #{dir}"
-  end
+def main
+  format_terraform_code
+  format_ruby_code
+  commit_changes "Commit changes made by code formatters"
 end
 
-def format_ruby_code
-  ruby_files_in_pr.each do |file|
-    execute "standardrb --fix #{file}" if FileTest.exists?(file)
-  end
-end
-
-def terraform_directories_in_pr
-  terraform_files_in_pr
-    .map { |f| File.dirname(f) }
-    .sort
-    .uniq
-end
-
-def ruby_files_in_pr
-  files_in_pr.grep(/\.rb$/)
-end
-
-def terraform_files_in_pr
-  files_in_pr.grep(/\.tf$/)
-end
-
-############################################################
-
-format_terraform_code
-format_ruby_code
-commit_changes "Commit changes made by code formatters"
+main
---
+++ shared/code_formatter.rb
@@ -0,0 +1,28 @@
+
+def format_terraform_code
+  terraform_directories_in_pr.each do |dir|
+    execute "terraform fmt #{dir}"
+  end
+end
+
+def format_ruby_code
+  ruby_files_in_pr.each do |file|
+    execute "standardrb --fix #{file}" if FileTest.exists?(file)
+  end
+end
+
+def terraform_directories_in_pr
+  terraform_files_in_pr
+    .map { |f| File.dirname(f) }
+    .sort
+    .uniq
+end
+
+def ruby_files_in_pr
+  files_in_pr.grep(/\.rb$/)
+end
+
+def terraform_files_in_pr
+  files_in_pr.grep(/\.tf$/)
+end
+
