e618e26fc08dae1d0d3baecaa3be751b55157817
David Salgado <david@digitalronin.com>
2020-09-29 09:57:14 +0100

    Move code into `main` method

--- conftest-yaml/run-conftest.rb
+++ conftest-yaml/run-conftest.rb
@@ -6,6 +6,28 @@ require "octokit"
 
 require File.join(File.dirname(__FILE__), "github")
 
+def main
+  client = GithubClient.new
+
+  # Assume rego policies are in the ./policy directory, unless user supplied a
+  # different location
+  policy_dir = ENV.fetch("POLICY_DIR", "policy")
+
+  # Get any additional command-line options for conftest
+  conftest_options = ENV.fetch("CONFTEST_OPTIONS", "")
+
+  # We want to test all files, rather than exiting on the first failure, so that
+  # the user can see all problems reported in the log. So, we collect all the
+  # exit statuses of the conftest commands.
+  cmd_statuses = yaml_files_in_pr(client).map { |file|
+    cmd = "conftest test -p #{policy_dir} #{conftest_options} #{file}"
+    command_status(cmd)
+  }
+
+  # Fail the action if conftest failed any YAML files
+  exit 1 unless cmd_statuses.all?
+end
+
 # Attempt to parse all the yaml/yml files in a PR, aside from those with
 # 'secret' in the filename.  Files with 'secret' in the name are very often
 # git-crypted, and so would cause this action to fail.
@@ -24,22 +46,4 @@ def command_status(cmd)
   status.success?
 end
 
-client = GithubClient.new
-
-# Assume rego policies are in the ./policy directory, unless user supplied a
-# different location
-policy_dir = ENV.fetch("POLICY_DIR", "policy")
-
-# Get any additional command-line options for conftest
-conftest_options = ENV.fetch("CONFTEST_OPTIONS", "")
-
-# We want to test all files, rather than exiting on the first failure, so that
-# the user can see all problems reported in the log. So, we collect all the
-# exit statuses of the conftest commands.
-cmd_statuses = yaml_files_in_pr(client).map { |file|
-  cmd = "conftest test -p #{policy_dir} #{conftest_options} #{file}"
-  command_status(cmd)
-}
-
-# Fail the action if conftest failed any YAML files
-exit 1 unless cmd_statuses.all?
+main
