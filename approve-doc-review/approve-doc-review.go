bf64494ddbefb041f548c132ed472cfc5513fa2d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-06-28 14:40:32 +0100

    Create buffio function that scans the fileName and outputs

--- approve-doc-review/approve-doc-review.go
+++ approve-doc-review/approve-doc-review.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 )
 
 // Idea:
@@ -15,13 +16,30 @@ import (
 // - if the changes are <= 1 and the pr owner is ministryofjustice/webops
 // - approve the pr
 
+// fileName is created by a GitHub action that outputs a git diff to a file
+// and copies it to the container running this package.
 const fileName = "changes"
 
 func main() {
-	_, err := ioutil.ReadFile(fileName)
+	// read the file
+	file, err := os.Open(fileName)
 	if err != nil {
-		log.Fatalln("File wasn't found:", err)
-	} else {
-		fmt.Println("File was found")
+		log.Fatalf("Failed to open %s: %s", fileName, err)
 	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var text []string
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	file.Close()
+
+	for _, line := range text {
+		fmt.Println(line)
+	}
+	// for every line that starts with a + and not +++
+	// check to see if that line starts with last_reviewed_on:
 }
