a90e5dc4f3577d7a66200ea05cc8e56d0dd4100d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-03-08 17:44:13 +0000

    :sparkles: Enhance malformed YAML detection and rejection
    This commit introduces several changes aimed at improving the detection of malformed YAML files in PRs. Additions include:
    - Creation of .gitignore, formed.yaml, malformed.yaml, and requirements.txt files.
    - Changes in 'github_service.py' to now utilize the PyGithub library and handle a PR number argument.
    - Expansion of functionality in 'reject_malformed_yaml.py'; this script now rejects PRs that contain malformed YAML files and provides a detailed error message pointing out the problematic files. Environment variables are used for GitHub token and PR number retrieval.

--- malformed-yaml/github_service.py
+++ malformed-yaml/github_service.py
@@ -1,12 +1,20 @@
+from github import Github
 
 
 class GitHubService():
     """A class to interact with GitHub API."""
 
-    def __init__(self, github_token):
+    def __init__(self, github_token, pr_number: int):
         self.github_token = github_token
+        self.pr_number = pr_number
+        self.client = Github(self.github_token)
 
     def get_changed_files_from_pr(self):
+        self.client.
         return [
-            "malformed-yaml/invalid.yaml",
+            "malformed.yaml",
+            "formed.yaml",
         ]
+
+    def fail_pr(self, message: str):
+        return 1
--- malformed-yaml/reject_malformed_yaml.py
+++ malformed-yaml/reject_malformed_yaml.py
@@ -1,36 +1,48 @@
+import os
 
 import yaml
 from github_service import GitHubService as github_service
 
 
-def get_changed_files_from_pr():
-    return [
-        "malformed-yaml/invalid.yaml",
-    ]
-
-
-def malformed_yaml(changed_files: list) -> bool:
+def malformed_yaml(changed_files: list) -> set:
+    files = set()
     for file in changed_files:
         with open(file, "r") as f:
             try:
                 yaml.safe_load(f)
             except yaml.YAMLError:
-                return True
-    return False
+                files.add(file)
+    return files
 
 
-def get_github_token():
-    token = "1234567890"
-    return token
+def get_github_token() -> tuple:
+    pr_number = os.getenv("PR_NUMBER")
+    token = os.getenv("GITHUB_TOKEN")
+    if not token or not pr_number:
+        raise ValueError("No GITHUB_TOKEN or PR_NUMBER env var found. Please make this available via the github actions workflow\nhttps://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret.")
+    return token, pr_number
+
+
+def message(files: set):
+    msg = "The following files contain malformed YAML:\n -"
+    msg += "\n -".join(files)
+    return msg
 
 
 def does_pr_contain_malformed_yaml() -> bool:
-    github = github_service(get_github_token())
+    token, pr = get_github_token()
+    github = github_service(token, pr)
     changed_files = github.get_changed_files_from_pr()
-    if malformed_yaml(changed_files):
-        return False
-    return True
+    malformed_files = malformed_yaml(changed_files)
+    if malformed_files:
+        github.fail_pr(message(malformed_files))
+        print(f"PR contains malformed YAML: {malformed_files}")
+        return True
+    print("PR does not contain malformed YAML")
+    return False
 
 
 if __name__ == "__main__":
-    does_pr_contain_malformed_yaml()
+    if does_pr_contain_malformed_yaml():
+        # Lets exit with a non-zero status code to indicate failure to GitHub Actions
+        os._exit(1)
