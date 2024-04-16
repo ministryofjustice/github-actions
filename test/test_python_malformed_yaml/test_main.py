7aebb72030147592a596cdbccb18a6fe3cfeb910
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-04-16 16:40:05 +0100

    tidy names

--- test/test_python_malformed_yaml/test_main.py
+++ test/test_python_malformed_yaml/test_main.py
@@ -8,8 +8,8 @@ main = python_malformed_yaml_main.main
 def test_main_exception():
     with pytest.raises(Exception) as exc_info:   
         main()
-    malformed_files = [
+    expected_malformed_files = [
       "test_python_malformed_yaml/test_yaml_files/bad.yml",
       "test_python_malformed_yaml/test_yaml_files/bad.yaml"
     ]
-    assert all(file in str(exc_info.value) for file in malformed_files)
+    assert all(file in str(exc_info.value) for file in expected_malformed_files)
