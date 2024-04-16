02496e452f7b201bd6ce2e073c2a2cdcfae2b690
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-04-16 16:14:00 +0100

    rename test files to avoid order issues

--- test/test_python_malformed_yaml/test_main.py
+++ test/test_python_malformed_yaml/test_main.py
@@ -4,7 +4,7 @@ python_malformed_yaml_main = importlib.import_module("python-malformed-yaml.main
 main = python_malformed_yaml_main.main
 
 
-expected_exception = 'Malformed YAML detected:\n\ntest/test_python_malformed_yaml/test_yaml_files/bad_2.yml:\nwhile scanning a quoted scalar\n  in "test/test_python_malformed_yaml/test_yaml_files/bad_2.yml", line 2, column 14\nfound unexpected end of stream\n  in "test/test_python_malformed_yaml/test_yaml_files/bad_2.yml", line 3, column 1\n\ntest/test_python_malformed_yaml/test_yaml_files/bad_1.yml:\nwhile scanning a quoted scalar\n  in "test/test_python_malformed_yaml/test_yaml_files/bad_1.yml", line 2, column 6\nfound unexpected end of stream\n  in "test/test_python_malformed_yaml/test_yaml_files/bad_1.yml", line 3, column 1\n Please correct and resubmit this PR.'
+expected_exception = 'Malformed YAML detected:\n\ntest/test_python_malformed_yaml/test_yaml_files/bad.yml:\nwhile scanning a quoted scalar\n  in "test/test_python_malformed_yaml/test_yaml_files/bad.yml", line 2, column 6\nfound unexpected end of stream\n  in "test/test_python_malformed_yaml/test_yaml_files/bad.yml", line 3, column 1\n\ntest/test_python_malformed_yaml/test_yaml_files/bad.yaml:\nwhile scanning a quoted scalar\n  in "test/test_python_malformed_yaml/test_yaml_files/bad.yaml", line 2, column 14\nfound unexpected end of stream\n  in "test/test_python_malformed_yaml/test_yaml_files/bad.yaml", line 3, column 1\n Please correct and resubmit this PR.'
 
 def test_exception():
     with pytest.raises(Exception) as exc_info:   
