52284cb7c37fdea03f603579e174ca759f500ae0
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2024-04-16 09:43:32 +0100

    revert docker file

--- python-malformed-yaml/main.py
+++ python-malformed-yaml/main.py
@@ -2,13 +2,10 @@ import yaml
 import pathlib
 
 def main():
-  # get all yaml files in PR (not just new/modified)
+
   yml_files = [p for p in pathlib.Path(".").rglob('*') if p.suffix in [".yml", ".yaml"]]
-  # remove any under a secret directory (wha?)
   yml_files = [y for y in yml_files if "secret/" not in str(y)]
 
-
-  # try to safe parse yaml files
   malformed_yaml = []
   for y in yml_files:
     with open(y) as stream:
